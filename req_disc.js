d1e9ffca8c0cff4648828ad9f973eb192a1c7a2e
osfunapps <osfunapps@email.com>
2019-07-18 18:53:21 +0300

    s

--- req_disc.js
+++ req_disc.js
@@ -1,38 +1,46 @@
+const self = module.exports = {
+
+    desc: function (server_public_key2) {
+
 // fake added:
-server_public_key = '048f7e89decacffa46616ecc923e8c0621b43c3a208b9b0591eb2ef187b5d49b1d29db720c82699df9872baf34706adc13302fa3c2e99520699b0b728321fa1644'
+        var server_public_key = '048f7e89decacffa46616ecc923e8c0621b43c3a208b9b0591eb2ef187b5d49b1d29db720c82699df9872baf34706adc13302fa3c2e99520699b0b728321fa1644'
 
-const crypto = require('crypto');
-var sha512 = crypto.createHash("sha512");
+        const crypto = require('crypto');
+        var sha512 = crypto.createHash("sha512");
 
-var EC = require('elliptic').ec;
-var ec = new EC('p256');
+        var EC = require('elliptic').ec;
+        var ec = new EC('p256');
 
 // Generate keys
 
 // 1) generate random key pair
-var key_pair_random = ec.genKeyPair();
+        var key_pair_random = ec.genKeyPair();
 
 // 2) generate key pair from the public key
-var key_pair_public_key = ec.keyFromPublic(server_public_key, 'hex')
+        var key_pair_public_key = ec.keyFromPublic(server_public_key, 'hex')
 
 // 3) get the public key from the server key pair
-var enc_key = key_pair_public_key.getPublic()
+        var enc_key = key_pair_public_key.getPublic()
 
 // 4) add the encrypted key to the random key pair, by thus produce a shared someting
-var unparsed_shared_secret = key_pair_random.derive(enc_key);
+        var unparsed_shared_secret = key_pair_random.derive(enc_key);
 
 // 5) turn the shared something into a string (which is the secret)
-var derived_secret = Buffer.from(unparsed_shared_secret.toString(16), 'hex')
+        var derived_secret = Buffer.from(unparsed_shared_secret.toString(16), 'hex')
 
 // at this point, the derived secret is:
 // [221, 48, 59,107,78,219,127,153,90,88,162,154,109,155,234,171,205,170,137,190,52,58,167,18,203,92,173,51,13,93,196,79]
 
 
 // 6) get
-var public_key_client = key_pair_random.getPublic('hex')
+        var public_key_client = key_pair_random.getPublic('hex')
 
-console.log(derived_secret)
-console.log(public_key_client)
+        console.log(derived_secret)
+        console.log(public_key_client)
+        return public_key_client
+    }
+}
+
 // at this point, the public key client is:
 // 0477a1a28a3375ff38ee5586ee3642a91af06cedbbc2ca249aba293119f429d89786d6e0cc831ff817c08d9b177b728aecf9581476e030c3cd4207175c517bf7f0
 //
--- send.js
+++ send.js
@@ -1,1 +1,28 @@
-console.log("DS")
+var disc = require("./req_disc")
+var url = require('url');
+
+'use strict'
+let http = require('http');
+let server = http.createServer(function (req, res) {
+    if( req.method === 'GET' ) {
+        console.log(req.url)
+        console.log(req.parameter)
+
+        var parsed = url.parse(req.url, true);
+        console.log(parsed.host)
+        console.log(parsed.pathname)
+        console.log(parsed.search)
+        var obj = parsed.query
+        console.log(obj['year'])
+
+        var ans = disc.desc("2")
+
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end(ans.toString());
+    } else {
+        res.writeHead(405, {'Content-Type': 'text/plain'});
+        res.end('Method Not Allowed\n');
+    }
+});
+server.listen(8443);
+console.log('Server running on port 8443');
